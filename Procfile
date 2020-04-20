25bcbf5395550a3ac30fb02f5ca283834750471d
daler.bakhriev <Daler.Bakhriev@x5.ru>
2020-04-21 01:05:55 +0300

    first steps with deployment to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: bin/12_factor_app
---
+++ cmd/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"os"
+
+	"github.com/sirupsen/logrus"
+)
+
+func main() {
+	logrus.Info("Hello, World!!!")
+
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		logrus.Fatal("Port was not set")
+	}
+}
